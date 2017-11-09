4f7f277944f1556b583ec7abeb9f7aed3c9a8132
LISTINGS09 <brian.timlin@gmail.com>
2017-11-09 16:50:20 +0000

    v0.2

--- ZEI/config.cpp
+++ ZEI/config.cpp
@@ -8,9 +8,19 @@ class CfgPatches {
 			"A3_Modules_F",
 			"A3_Modules_F_Curator"
 		};
-		units[] = {ZEI_InteriorCiv};
+		units[] = {
+			ZEI_CivilianUrban, 
+			ZEI_fillObjectAny // Arguments aren't passed in Zeus, so can't specify fillType.
+		};
 		weapons[] = {};
       	worlds[] = {};
+	};
+};
+
+class CfgFactionClasses {
+	class NO_CATEGORY;
+	class zei_interiors: NO_CATEGORY {
+		displayName = "Interiors (ZEI)";
 	};
 };
 
@@ -20,11 +30,11 @@ class CfgVehicles {
 		class ModuleDescription;
 	};
 	
-	class ZEI_InteriorCiv: Module_F {
+	class ZEI_CivilianUrban : Module_F {
 		scope = 2;
 		scopeCurator = 2; 
-		displayName = "Interior - Civilian";
-		category = "Environment";
+		displayName = "Civilian (Urban)";
+		category = "zei_interiors";
 		function = "ZEI_fnc_interiorCiv";
 		functionPriority = 1;
 		isGlobal = 0;
@@ -37,6 +47,111 @@ class CfgVehicles {
 			sync[] = {};
 		};
 	};
+	
+	class ZEI_fillObjectFood : Module_F {
+		scope = 2;
+		scopeCurator = 2; 
+		displayName = "Object Fill (Food)";
+		category = "zei_interiors";
+		function = "ZEI_fnc_fillObject";
+		functionPriority = 1;
+		isGlobal = 0;
+		isTriggerActivated = 0;
+		is3DEN = 1;
+		
+		class Arguments {
+            class fillType {
+				displayName = "Fill Type";
+				description = "Items to spawn";
+                typeName = "STRING";
+                defaultValue = "FOOD";
+            };
+		};
+	};
+	
+	class ZEI_fillObjectDesk : Module_F {
+		scope = 2;
+		scopeCurator = 2; 
+		displayName = "Object Fill (Office)";
+		category = "zei_interiors";
+		function = "ZEI_fnc_fillObject";
+		functionPriority = 1;
+		isGlobal = 0;
+		isTriggerActivated = 0;
+		is3DEN = 1;
+		
+		class Arguments {
+            class fillType {
+				displayName = "Fill Type";
+				description = "Items to spawn";
+                typeName = "STRING";
+                defaultValue = "DESK";
+            };
+		};
+	};
+	
+	class ZEI_fillObjectHeal : Module_F {
+		scope = 2;
+		scopeCurator = 2; 
+		displayName = "Object Fill (Medical)";
+		category = "zei_interiors";
+		function = "ZEI_fnc_fillObject";
+		functionPriority = 1;
+		isGlobal = 0;
+		isTriggerActivated = 0;
+		is3DEN = 1;
+		
+		class Arguments {
+            class fillType {
+				displayName = "Fill Type";
+				description = "Items to spawn";
+                typeName = "STRING";
+                defaultValue = "HEAL";
+            };
+		};
+	};
+	
+	class ZEI_fillObjectTool : Module_F {
+		scope = 2;
+		scopeCurator = 2; 
+		displayName = "Object Fill (Tools)";
+		category = "zei_interiors";
+		function = "ZEI_fnc_fillObject";
+		functionPriority = 1;
+		isGlobal = 0;
+		isTriggerActivated = 0;
+		is3DEN = 1;
+		
+		class Arguments {
+            class fillType {
+				displayName = "Fill Type";
+				description = "Items to spawn";
+                typeName = "STRING";
+                defaultValue = "TOOL";
+            };
+		};
+	};
+	
+	class ZEI_fillObjectAny : Module_F {
+		scope = 2;
+		scopeCurator = 2; 
+		displayName = "Object Fill (Random)";
+		category = "zei_interiors";
+		function = "ZEI_fnc_fillObject";
+		functionPriority = 1;
+		isGlobal = 0;
+		isTriggerActivated = 0;
+		is3DEN = 1;
+		
+		class Arguments {
+            class fillType {
+				displayName = "Fill Type";
+				description = "Items to spawn";
+                typeName = "STRING";
+                defaultValue = "ANY";
+            };
+		};
+	};
 };
 
 class CfgFunctions {
@@ -44,6 +159,7 @@ class CfgFunctions {
 		class Interiors {
 			file = "\zei\functions";
 			class interiorCiv{};
+			class fillObject{};
 			//class interiorMil{};
 		};
 	};
