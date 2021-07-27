5c737bbfd27ba8a837847b4fab96ea9b41380b2b
LISTINGS09 <brian.timlin@gmail.com>
2021-07-27 11:50:52 +0100

    v1.16
    File update

--- mod.cpp
+++ mod.cpp
@@ -2,7 +2,6 @@ name = "[ZEI] Zeus and Eden Interiors";
 picture = "zeus.paa";
 actionName = "Website";
 action = "http://zeus-community.net/";
-tooltip = "ZEI 1.13";
 description = "[ZEI] Zeus and Eden Interiors";
 overview = "ZEI adds interiors to buildings.";
 author = "2600K/LSD";
